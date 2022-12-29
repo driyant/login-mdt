d805428136c47e44322852c8e7d390ef36dd9cfe
A.A Ryanta <message.driyant@outlook.com>
2022-12-29 11:12:54 +0700

    Add basic function login feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passowrdIsInvalid, setPasswordIsInvalid] = useState(false);
+  const loginHandler = (e) => {
+    e.preventDefault();
+    if (
+      password.includes("1") ||
+      password.includes("2") ||
+      password.includes("3") ||
+      password.includes("4") ||
+      password.includes("5") ||
+      password.includes("6") ||
+      password.includes("7") ||
+      password.includes("8") ||
+      password.includes("9") ||
+      password.includes("0")
+    ) {
+      setPasswordIsInvalid(true);
+      return;
+    }
+    const data = {
+      email: email,
+      password: password,
+    };
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((resp) => {
+        if (!resp.ok) {
+          setEmailIsInvalid(true);
+        } else {
+          setEmailIsInvalid(false);
+          setPasswordIsInvalid(false);
+          return resp.json();
+        }
+      })
+      .then((data) => console.log(data));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        onSubmit={loginHandler}
+        style={{
+          marginTop: "1rem",
+          display: "flex",
+          flexFlow: "column",
+          justifyContent: "space-around",
+        }}
+      >
+        <div className="form-group" style={{ marginBottom: "1rem" }}>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {emailIsInvalid && <p style={{ color: "red" }}>Email is invalid!</p>}
+        </div>
+        <div className="form-group" style={{ marginBottom: "1rem" }}>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {passowrdIsInvalid && <p style={{ color: "red" }}>Password is invalid!</p>}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
